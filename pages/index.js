aa7219f519e39e953f19d1bea38cade5bdc9a5cb
SadCatto <rushimalu44@gmail.com>
2023-05-22 17:16:20 +0530

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,196 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { redirect } from "next/dist/server/api-utils"
+import { useRouter } from "next/router"
+import { useState } from "react"
 
-const inter = Inter({ subsets: ['latin'] })
-
+/* eslint-disable @next/next/no-img-element */
 export default function Home() {
+  const router = useRouter()
+  const [cardNum, setCardNum] = useState('0000 0000 0000 0000')
+  const [holderName, setholderName] = useState('JANE APPLESEED')
+  const [expMonth, setexpMonth] = useState(null)
+  const [expYear, setexpYear] = useState(null)
+  const [CVC, setCVC] = useState(null)
+  const [cardError, setcardError] = useState(null)
+  const [monthError, setMonthError] = useState(null)
+  const [CVCError, setCVCError] = useState(null)
+  const validation = (ev) => {
+    ev.preventDefault()
+    let correct = true
+    if (cardNum.match("[a-zA-Z]+")) {
+      setcardError('Wrong format, numbers only')
+      correct = false
+    } else {
+      setcardError(null)
+    }
+    if (expMonth == null || expYear == null) {
+      setMonthError(`Can't be blank`)
+      correct = false
+    } else {
+      setMonthError(null)
+    }
+    if (CVC == null) {
+      setCVCError(`Can't be blank`)
+      correct = false
+    } else {
+      setCVCError(null)
+    }
+    if (correct == true) {
+      router.push({
+        pathname: '/success',
+        query: {
+          holderName: holderName,
+          cardNum: cardNum,
+          expMonth: expMonth,
+          expYear: expYear,
+          cvc: CVC
+        }
+      }, '/success');
+    } else {
+      return false
+    }
+  }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main >
+      <div className="hidden lg:flex lg:flex-row lg:place-content-evenly lg:h-screen lg:w-screen lg:bg-[url('/images/bg-main-desktop.png')] lg:bg-no-repeat">
+        <div className="flex flex-col justify-center items-center gap-8 w-3/6">
+          <div className="rounded-md h-2.5/6 w-max bg-no-repeat flex align-start">
+            <div className="bg-[url('/images/bg-card-front.png')] bg-no-repeat flex flex-col h-[15.5rem] w-[28rem] place-content-evenly items-start pl-8 pr-8">
+              <img src="/images/card-logo.svg" alt="logo" className="h-fit w-fit"></img>
+              <div className="flex flex-col gap-7 w-full">
+                <div className="w-full text-center text-[24px] text-white tracking-[0.25em]">
+                  {cardNum}
+                </div>
+                <div className="flex items-start place-content-between w-full text-[12px] text-white tracking-[0.25em]">
+                  <div>
+                    {holderName}
+                  </div>
+                  <div>
+                    {expMonth || '00'} /{expYear || '00'}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full max-w-[80%] flex justify-end">
+            <div className="flex bg-[url('/images/bg-card-back.png')] bg-no-repeat rounded-md">
+              <div className="flex justify-end items-center h-[15rem] w-[28rem] pr-14">
+                {CVC}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-start w-3/6 justify-center">
+          <form onSubmit={event => validation(event)} className="flex flex-col gap-4 w-full max-w-[60%] ml-12">
+            <div className="w-full flex gap-4 flex-col">
+              <div className="flex flex-col gap-2">
+                <label>CARDHOLDER NAME</label>
+                <input className="border-2 rounded-lg p-2" placeholder="e.g. Jane Appleseed" onChange={ev => setholderName(ev.target.value)}></input>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label>CARDNUMBER</label>
+                <input className={cardError ? 'border-2 border-rose-500 rounded-lg p-2' : 'border-2 rounded-lg p-2'} placeholder="e.g. 1234 5678 9123 0000" maxLength={19} onChange={ev => setCardNum(ev.target.value)}></input >
+                <div className={cardError ? 'text-rose-500 text-xs' : 'hidden'}>
+                  {cardError}
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full gap-2 place-content-evenly">
+              <div className="flex flex-col gap-2 w-full max-w-[50%]">
+                <div>
+                  <label>EXP. DATE (MM/YY)</label>
+                </div>
+                <div className="flex gap-2">
+                  <input className={monthError ? 'border-2 border-rose-500 rounded-lg p-2 w-full' : 'border-2 rounded-lg p-2 w-full'} placeholder="MM" onChange={ev => setexpMonth(ev.target.value)}></input>
+                  <input className={monthError ? 'border-2 border-rose-500 rounded-lg p-2 w-full' : 'border-2 rounded-lg p-2 w-full'} placeholder="YY" onChange={ev => setexpYear(ev.target.value)}></input>
+                </div>
+                <div className={monthError ? 'text-rose-500 text-xs' : 'hidden'}>
+                  {monthError}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <div>
+                  <label>CVC</label>
+                </div>
+                <div className="flex w-full">
+                  <input className={CVCError ? 'border-2 border-rose-500 rounded-lg p-2' : 'border-2 rounded-lg p-2'} placeholder="e.g. 123" onChange={ev => setCVC(ev.target.value)}></input>
+                </div>
+                <div className={CVCError ? 'text-rose-500 text-xs' : 'hidden'}>
+                  {CVCError}
+                </div>
+              </div>
+            </div>
+            <div className="flex w-full justify-center">
+              <button className="bg-[#21092f] p-2 m-2 text-white rounded-lg w-full">CONFIRM</button>
+            </div>
+          </form>
+        </div>
+      </div >
+      {/* Mobile Part  */}
+      <div className="flex flex-col gap-2 items-center justify-center lg:hidden">
+        <div className="bg-[url('/images/bg-main-mobile.png')] h-[14rem] w-full p-4 flex flex-col items-end justify-center bg-no-repeat bg-cover">
+          <div className="bg-[url('/images/bg-card-back.png')] w-full max-w-[80%] h-[9.5rem] bg-no-repeat bg-contain z-0 relative flex items-center">
+            <div className="text-sm w-full max-w-[90%] h-[15%] flex justify-end text-white items-center pr-2 pb-1">
+              {CVC || '000'}
+            </div>
+          </div>
+          <div className="w-full z-10 absolute pt-44 flex justify-center">
+            <div className="bg-[url('/images/bg-card-front.png')] w-full max-w-[80%] h-[9.5rem] bg-no-repeat bg-contain z-10 flex flex-col place-content-between p-4">
+              <div className="bg-[url('/images/card-logo.svg')] w-[20%] h-[2rem] bg-contain bg-no-repeat"></div>
+              <div className="flex flex-col w-full max-w-[92%]  text-sm gap-4 pl-2 pr-2">
+                <div className="w-full text-center text-white tracking-[0.15em]">
+                  {cardNum}
+                </div>
+                <div className="flex items-start place-content-between w-full text-white tracking-[0.15em]">
+                  <div className="text-xs">
+                    {holderName}
+                  </div>
+                  <div className="text-xs">
+                    {expMonth || '00'} /{expYear || '00'}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-16">
+          <form onSubmit={event => validation(event)} className="flex flex-col gap-4 w-full items-center justify-center p-4">
+            <div className="w-full flex gap-4 flex-col">
+              <div className="flex flex-col gap-2">
+                <label>CARDHOLDER NAME</label>
+                <input className="border-2 rounded-lg p-2" placeholder="e.g. Jane Appleseed" onChange={ev => setholderName(ev.target.value)}></input>
+              </div>
+              <div className="flex flex-col gap-2">
+                <label>CARDNUMBER</label>
+                <input className={cardError ? 'border-2 border-rose-500 rounded-lg p-2' : 'border-2 rounded-lg p-2'} placeholder="e.g. 1234 5678 9123 0000" maxLength={19} onChange={ev => setCardNum(ev.target.value)}></input >
+                <div className={cardError ? 'text-rose-500 text-xs' : 'hidden'}>
+                  {cardError}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 w-full items-center ">
+              <div className="flex items-start w-full justify-start gap-8">
+                <label>EXP. DATE (MM/YY)</label>
+                <label>CVC</label>
+              </div>
+              <div className="flex gap-2">
+                <div className="w-full flex gap-2">
+                  <input className={monthError ? 'border-2 border-rose-500 rounded-lg p-2 w-[100%]' : 'border-2 rounded-lg p-2 w-[100%] max-w-[80%]'} placeholder="MM" onChange={ev => setexpMonth(ev.target.value)}></input>
+                  <input className={monthError ? 'border-2 border-rose-500 rounded-lg p-2 w-[100%]' : 'border-2 rounded-lg p-2 w-[100%] max-w-[80%]'} placeholder="YY" onChange={ev => setexpYear(ev.target.value)}></input>
+                </div>
+                <div className="w-full">
+                  <input className={CVCError ? 'border-2 border-rose-500 rounded-lg w-full p-2' : 'border-2 w-full rounded-lg p-2'} placeholder="e.g. 123" onChange={ev => setCVC(ev.target.value)}></input>
+                </div>
+              </div>
+            </div>
+            <div className={CVCError ? 'text-rose-500 text-xs' : 'hidden'}>
+              {CVCError}
+            </div>
+            <div className="flex w-full justify-center">
+              <button className="bg-[#21092f] p-2 m-2 text-white rounded-lg w-full">CONFIRM</button>
+            </div>
+          </form>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </main >
   )
 }
---
+++ pages/success.js
@@ -0,0 +1,80 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+
+
+export default function Success(props) {
+    const [cardNum, setCardNum] = useState('0000 0000 0000 0000')
+    const [holderName, setholderName] = useState('JANE APPLESEED')
+    const [expMonth, setexpMonth] = useState(null)
+    const [expYear, setexpYear] = useState(null)
+    const [CVC, setCVC] = useState(null)
+    const router = useRouter()
+    useEffect(() => {
+        setholderName(router.query.holderName);
+        setCardNum(router.query.cardNum);
+        setexpMonth(router.query.expMonth);
+        setexpYear(router.query.expYear);
+        setCVC(router.query.cvc);
+    }, [router]);
+
+    const home = () => {
+        router.push('/')
+    }
+    return (
+        <main>
+            <div className="hidden lg:flex lg:flex-row lg:place-content-evenly lg:h-screen lg:w-screen lg:bg-[url('/images/bg-main-desktop.png')] lg:bg-no-repeat">
+                <div className="flex flex-col justify-center items-center gap-8 w-3/6">
+                    <div className="rounded-md h-2.5/6 w-max bg-no-repeat flex align-start">
+                        <div className="bg-[url('/images/bg-card-front.png')] bg-no-repeat flex flex-col h-[15.5rem] w-[28rem] place-content-evenly items-start pl-8 pr-8">
+                            <img src="/images/card-logo.svg" alt="logo" className="h-fit w-fit"></img>
+                            <div className="flex flex-col gap-7 w-full">
+                                <div className="w-full text-center text-[24px] text-white tracking-[0.25em]">
+                                    {cardNum}
+                                </div>
+                                <div className="flex items-start place-content-between w-full text-[12px] text-white tracking-[0.25em]">
+                                    <div>
+                                        {holderName}
+                                    </div>
+                                    <div>
+                                        {expMonth || '00'} /{expYear || '00'}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full max-w-[80%] flex justify-end">
+                        <div className="flex bg-[url('/images/bg-card-back.png')] rounded-md">
+                            <div className="flex justify-end items-center h-[15rem] w-[28rem] pr-14">
+                                {CVC}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col items-start justify-center w-3/6">
+                    <div className="w-full max-w-[60%] flex flex-col gap-4 items-center">
+                        <div>
+                            <img src="/images/icon-complete.svg" alt="complete thumbs up" className="h-fit w-fit"></img>
+                        </div>
+                        <div></div>
+                        <div className="text-3xl tracking-widest">
+                            <h1>THANK YOU!</h1>
+                        </div>
+                        <div className="text-lg text-[#8e8593]">
+                            <h2>We've added your card details</h2>
+                        </div>
+                        <div className="flex items-center justify-center w-full">
+                            <button className="bg-[#21092f] p-2 text-white font-thin rounded-lg w-5/6" onClick={home}>Continue</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            {/* Mobile Part  */}
+            <div className="flex items-center justify-center lg:hidden">
+                BOKACHODAAAAAAAAAAAA
+            </div>
+        </main>
+    )
+}
