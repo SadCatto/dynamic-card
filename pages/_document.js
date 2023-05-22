216a16bccba58ca01166c22836eb5fc3df1b5afb
SadCatto <rushimalu44@gmail.com>
2023-05-22 17:38:09 +0530

    updated title

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" title='interactive-card-home'>
+      <title>Interactive card Form</title>
       <Head />
       <body>
         <Main />
